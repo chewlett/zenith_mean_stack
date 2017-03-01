fab373051f33065b9881a4ca9e3dc4beab2d0dc6
Tallis Hobbs <rhtallis@gmail.com>
2017-03-01 15:47:57 -0800

    various chagnes

---
+++ routes/activities_api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var mongojs = require("mongojs");
+var theDB = require("../db");
+
+// list all activities
+router.get()
+
+module.exports = router;
